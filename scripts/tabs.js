d7b748bf2d303e1c22ea00b663bb31fc48b610ee
FLekGerrera <126162593+FLekGerrera@users.noreply.github.com>
2026-08-17 16:19:38 +0500

    добавлен js в секцию reviews

--- scripts/tabs.js
+++ scripts/tabs.js
@@ -1,3 +1,19 @@
-$(".interactive-avatar__link").click(e => {
+const findBlockByAlias = alias => {
+    return $(".reviews__dispaly-inner").filter((ndx, item) => {
+        return $(item).attr("data-switcher-with") == alias;
+    })
+}
+
+$(".reviews__switcher-item").on('click', e => {
     e.preventDefault();
-})
+
+    console.log(e.currentTarget)
+
+    const $this = $(e.currentTarget);
+    const target = $this.attr("data-open");
+    const itemToShow = findBlockByAlias(target);
+    const curItem = $this.closest(".reviews__switcher-item");
+    
+    itemToShow.addClass("reviews__dispaly-inner--active").siblings().removeClass("reviews__dispaly-inner--active");
+    curItem.addClass("interactive-avatar--active").siblings().removeClass("interactive-avatar--active");
+});
